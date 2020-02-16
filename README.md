9779ad9f29d60ad64de33accb37a0f6f58f749ba
Alex Curran <alexcurran.va@gmail.com>
2020-02-16 12:09:10 -0600

    Updated triangle to display multiple colors

---
+++ README.md
@@ -0,0 +1,7 @@
+# WebGL Triangle
+
+[Link](https://apcurran.github.io/webgl2-first-triangle/)
+
+## Features:
+
+* Colorized triangle
--- script.js
+++ script.js
@@ -14,18 +14,34 @@ const vertexData = [
     -1, -1, 0
 ];
 
-const buffer = gl.createBuffer();
+const colorData = [
+    1, 0, 0, // Vertex 1 color
+    0, 1, 0, // Vertex 2 color
+    0, 0, 1  // Vertex 3 color
+];
 
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+const positionBuffer = gl.createBuffer();
+
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+const colorBuffer = gl.createBuffer();
+
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 
 gl.shaderSource(vertexShader, `
+    precision mediump float;
+
     attribute vec3 position;
+    attribute vec3 color;
+    varying vec3 vColor;
 
     void main()
     {
+        vColor = color;
         gl_Position = vec4(position, 1);
     }
 `);
@@ -34,9 +50,13 @@ gl.compileShader(vertexShader);
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 
 gl.shaderSource(fragmentShader, `
+    precision mediump float;
+    
+    varying vec3 vColor;
+
     void main()
     {
-        gl_FragColor = vec4(0, 0, 1, 1);
+        gl_FragColor = vec4(vColor, 1);
     }
 `);
 gl.compileShader(fragmentShader);
@@ -50,7 +70,14 @@ gl.linkProgram(program);
 const positionLocation = gl.getAttribLocation(program, `position`);
 
 gl.enableVertexAttribArray(positionLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+const colorLocation = gl.getAttribLocation(program, `color`);
+
+gl.enableVertexAttribArray(colorLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
 
 gl.useProgram(program);
 gl.drawArrays(gl.TRIANGLES, 0, 3);
